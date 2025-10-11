ba5c2479632c0410140564e34669570954a2cce0
Farouk Bakari <farouk.baccari.externe@emeria.eu>
2025-10-11 22:07:01 +0200

    clean: old websocket implementation to prepare for fastws

--- backend/src/trading_api/models/__init__.py
+++ backend/src/trading_api/models/__init__.py
@@ -7,21 +7,10 @@ organized by functionality:
 - market/: Market data, instruments, quotes, and bars
 - trading/: Orders, positions, and trading operations
 - account/: User accounts, authentication, and balances
-- realtime/: WebSocket messages and real-time streams
 """
 
-from typing import Union
-
-# Import from account domain
-from .account import AuthenticationMessage, AuthenticationResponse, BalanceUpdate
-
 # Import from common utilities
-from .common import (
-    ErrorResponse,
-    WebSocketMessage,
-    WebSocketResponse,
-    WebSocketSubscription,
-)
+from .common import ErrorResponse
 
 # Import from market data domain
 from .market import (
@@ -40,47 +29,9 @@ from .market import (
     SymbolInfo,
 )
 
-# Import from realtime domain
-from .realtime import (
-    CandlestickUpdate,
-    ChannelConfig,
-    ChannelStatus,
-    HeartbeatMessage,
-    MarketDataTick,
-    OrderBookUpdate,
-    SubscriptionConfirmation,
-    SystemMessage,
-    TradeUpdate,
-    TradingNotification,
-    WebSocketServerConfig,
-)
-
-# Import from trading domain
-from .trading import OrderUpdate, PositionUpdate
-
-WebSocketMessageUnion = Union[
-    MarketDataTick,
-    OrderBookUpdate,
-    TradeUpdate,
-    BalanceUpdate,
-    PositionUpdate,
-    OrderUpdate,
-    TradingNotification,
-    CandlestickUpdate,
-    SystemMessage,
-    HeartbeatMessage,
-    AuthenticationMessage,
-    AuthenticationResponse,
-    SubscriptionConfirmation,
-    ChannelStatus,
-]
-
 __all__ = [
     # Common utilities
     "ErrorResponse",
-    "WebSocketMessage",
-    "WebSocketResponse",
-    "WebSocketSubscription",
     # Market data models
     "Bar",
     "DatafeedConfiguration",
@@ -95,24 +46,4 @@ __all__ = [
     "SearchSymbolResultItem",
     "SearchSymbolsRequest",
     "SymbolInfo",
-    # Trading models
-    "OrderUpdate",
-    "PositionUpdate",
-    # Account models
-    "AuthenticationMessage",
-    "AuthenticationResponse",
-    "BalanceUpdate",
-    # Real-time models
-    "CandlestickUpdate",
-    "ChannelConfig",
-    "ChannelStatus",
-    "HeartbeatMessage",
-    "MarketDataTick",
-    "OrderBookUpdate",
-    "SubscriptionConfirmation",
-    "SystemMessage",
-    "TradeUpdate",
-    "TradingNotification",
-    "WebSocketMessageUnion",
-    "WebSocketServerConfig",
 ]
